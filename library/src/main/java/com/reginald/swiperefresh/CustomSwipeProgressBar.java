afac0a213511455f7a1c80d80f1d5991067621dd
stefan.strobel <stefan.strobel@xcom.de>
2019-04-26 09:32:03 +0200

    fix: Issue #16 - Invalid Layer Save Flag - only ALL_SAVE_FLAGS is allowed
    This fix is required to update to Android SDK 28.

--- library/src/main/java/com/reginald/swiperefresh/CustomSwipeProgressBar.java
+++ library/src/main/java/com/reginald/swiperefresh/CustomSwipeProgressBar.java
@@ -13,6 +13,8 @@ import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.view.animation.Interpolator;
 
+import static android.graphics.Canvas.ALL_SAVE_FLAG;
+
 
 /**
  * Custom progress bar that shows a cycle of colors as widening circles that
@@ -155,7 +157,7 @@ final class CustomSwipeProgressBar {
                 // Radius of the circle is half of the screen.
                 float clearRadius = width / 2 * INTERPOLATOR.getInterpolation(pct);
                 mClipRect.set(cx - clearRadius, mBounds.top, cx + clearRadius, mBounds.bottom);
-                canvas.saveLayerAlpha(mClipRect, 0, 0);
+                canvas.saveLayerAlpha(mClipRect, 0, ALL_SAVE_FLAG);
                 // Only draw the trigger if there is a space in the center of
                 // this refreshing view that needs to be filled in by the
                 // trigger. If the progress view is just still animating, let it
